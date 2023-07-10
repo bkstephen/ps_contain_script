382bafe6ac680585f239f8c1ed01b9f58374a019
Stef B <8013595+bkstephen@users.noreply.github.com>
2023-07-10 16:01:46 +0100

    Update contain.ps1

--- contain.ps1
+++ contain.ps1
@@ -19,9 +19,26 @@ if ( $reply -eq 'y' ) {
     Start-MpWDOScan
 }
 
+$reply = Read-Host -Prompt "Restore from restore point?[y/n]"
+# revert with "bcdedit /deletevalue {current} safeboot"
+if ( $reply -eq 'y' ) { 
+    Get-ComputerRestorePoint
+    Restore-Computer
+}
+
 $reply = Read-Host -Prompt "Restart in safemode?[y/n]"
 # revert with "bcdedit /deletevalue {current} safeboot"
 if ( $reply -eq 'y' ) { 
     bcdedit /set {current} safeboot minimal 
     shutdown /r
 }
+
+$reply = Read-Host -Prompt "Wipe everything?[y/n]"
+# revert with "bcdedit /deletevalue {current} safeboot"
+if ( $reply -eq 'y' ) { 
+    systemreset -factoryreset
+}
+
+# Other commands:
+# Get-ComputerRestorePoint
+# Restore-Computer -RestorePoint 255
